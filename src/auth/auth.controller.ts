1ac4bc179046eca3f13edb9dfac00f04a687e686
victoradepoju <victoradepoju30@gmail.com>
2023-06-30 20:58:15 +0100

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post('signup')
+    signup(){
+        return 'I am signed up'
+    }
+
+    @Post('signin')
+    signin(){
+        return 'I am signed in'
+    }
+}
